9d6f3ed2062ec8a8b6ec8969949ced967a26d761
Peter Stegarus <petre-florin.stegru@softwire.com>
2022-06-30 10:17:27 +0100

    bishop can move diagonally

--- Chessington.GameEngine/Pieces/Bishop.cs
+++ Chessington.GameEngine/Pieces/Bishop.cs
@@ -1,4 +1,5 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using System.Linq;
 
 namespace Chessington.GameEngine.Pieces
@@ -6,11 +7,47 @@ namespace Chessington.GameEngine.Pieces
     public class Bishop : Piece
     {
         public Bishop(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
+
+        private bool ValidSquare(Square square)
+        {
+            var row = square.Row;
+            if (row < 0 || row > 7) return false;
+
+            var col = square.Col;
+            if (col < 0 || col > 7) return false;
+
+            return true;
+        }
+
+        private List<Square> PrimaryDiagonalMoves(Square currentSquare)
+        {
+            var diagonalOffset = currentSquare.Row - currentSquare.Col;
+
+            return Enumerable.Range(0, 8)
+                .Select(x => new Square(x + diagonalOffset, x))
+                .Where(ValidSquare).ToList();
+        }
+
+        private List<Square> SecondaryDiagonalMoves(Square currentSquare)
+        {
+            var diagonalSum = currentSquare.Row + currentSquare.Col;
+
+            return Enumerable.Range(0, 8)
+                .Select(x => new Square(x, diagonalSum - x))
+                .Where(ValidSquare).ToList();
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            Square currentSquare = board.FindPiece(this);
+
+            var possibleMoves = PrimaryDiagonalMoves(currentSquare);
+            possibleMoves.AddRange(SecondaryDiagonalMoves(currentSquare));
+
+            return possibleMoves.Where(square => square != currentSquare);
         }
     }
 }
