aa99ed995216174206cc7d695e21a995332e033e
Peter Stegarus <petre-florin.stegru@softwire.com>
2022-06-30 09:37:47 +0100

    rook can move laterally

--- Chessington.GameEngine/Pieces/Rook.cs
+++ Chessington.GameEngine/Pieces/Rook.cs
@@ -6,11 +6,30 @@ namespace Chessington.GameEngine.Pieces
     public class Rook : Piece
     {
         public Rook(Player player)
-            : base(player) { }
+            : base(player)
+        {
+        }
+
+        private List<Square> HorizontalMoves(Square currentSquare)
+        {
+            return Enumerable.Range(0, 8).Where(x => x != currentSquare.Col)
+                .Select(col => new Square(currentSquare.Row, col)).ToList();
+        }
+
+        private List<Square> VerticalMoves(Square currentSquare)
+        {
+            return Enumerable.Range(0, 8).Where(x => x != currentSquare.Row)
+                .Select(row => new Square(row, currentSquare.Col)).ToList();
+        }
 
         public override IEnumerable<Square> GetAvailableMoves(Board board)
         {
-            return Enumerable.Empty<Square>();
+            var currentSquare = board.FindPiece(this);
+
+            var possibleMoves = HorizontalMoves(currentSquare);
+            possibleMoves.AddRange(VerticalMoves(currentSquare));
+
+            return possibleMoves;
         }
     }
 }
